1b633037c7834c4e5aecb4d1e4d60e7cf0eb9a0d
Eric <nostaff@sina.com>
2017-06-21 10:42:18 +0800

    更新小红点

--- src/components/TabBarItem.vue
+++ src/components/TabBarItem.vue
@@ -1,9 +1,9 @@
 <template>
     <a href="javascript:;;" class="tab-item" :class="{'active': isActive}" @click="onItemClick(true)">
-        <div :class="['icon', {'reddot': showDot}]" v-if="!simple">
+        <div class="icon" v-if="!simple">
             <slot name="icon" v-if="!simple && !(hasActiveIcon && isActive)"></slot>
             <slot name="icon-active" v-if="!simple && hasActiveIcon && isActive"></slot>
-            <span v-if="badge" v-text="badge" :class="['badge', {'badge-single': typeof badge !== 'undefined' && badge.toString().length === 1}]"></span>
+            <span v-if="badge || showDot" v-text="badge" :class="['badge', {'badge-dot': typeof badge === 'undefined', 'badge-single': typeof badge !== 'undefined' && badge.toString().length === 1}]"></span>
         </div>
         <slot name="label"></slot>
     </a>
