9bcf135b72b3b0a420d092a7bd528326ea21aa30
Eric <nostaff@sina.com>
2017-10-02 00:22:06 +0800

    no message

--- src/demos/tabbar/PageFromTabbar.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <ion-page v-nav="{title: '另一个页面', showBackButton: true}">
-    <ion-content class="padding padding-top">
-      <p class="text-center">没错，我就是另一个页面</p>
-    </ion-content>
-  </ion-page>
-</template>
