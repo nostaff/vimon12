c87889d033bf0b2b9c8977040f037f6093d2f843
Eric <nostaff@sina.com>
2017-12-07 22:31:15 +0800

    fix: radio 移除checked属性

--- src/components/radio/radio.vue
+++ src/components/radio/radio.vue
@@ -1,12 +1,74 @@
 <template>
   <div :class="[themeClass, colorClass, {'radio-disabled':disabled}]">
-    <div :class="['radio-icon', isChecked?'radio-checked':'']">
+    <div :class="['radio-icon', {'radio-checked':isChecked}]">
       <div class="radio-inner"></div>
     </div>
     <ion-button role="radio" :disabled="disabled" @click.native="onChecked"></ion-button>
   </div>
 </template>
 <script>
+/**
+ * @component Radio
+ * @description
+ *
+ * ## 表单组件 / Radio单选框组件
+ *
+ * ### 注意
+ *
+ * 使用v-modal切换状态(数据控制), 不支持checked属性, value为纯string字段, 用于唯一标识;
+ *
+ * ### 其他
+ *
+ * - 组件支持异步操作实例化
+ * - 目前组件的层级嵌套关系如下:  List -> Item -> Radio
+ * - 动态设置了单个radio的禁用状态, 如果之前是选中的则取消选中, 手动点击和数据选中都会生效
+ *
+ * ### 如何引入
+ * ```
+ * // 引入
+ * import {Radio} from 'vimo'
+ * // 安装
+ * Vue.component(Radio.name, Radio)
+ * // 或者
+ * export default{
+ *   components: {
+ *    Radio
+ *  }
+ * }
+ * ```
+ *
+ * @props {String} [mode='ios'] - 模式
+ * @props {String} [color] - 颜色
+ * @props {Boolean} [disabled=false] - 单向选择, 点击且换并不对父组件传递
+ *
+ * @fire component:Radio#onSelect - 点按选择时触发
+ * @demo #/radio
+ * @usage
+ * <List radio-group v-model="fruits" :disabled="isListDisabled">
+ *    <ListHeader>Fruits</ListHeader>
+ *    <Item>
+ *        <Label>Apple</Label>
+ *        <Radio value="apple" :disabled="isAppleDisabled" @onSelect="onSelectHandler"></Radio>
+ *    </Item>
+ *    <Item>
+ *        <Label>Banana</Label>
+ *        <Radio value="banana" color="danger" @onSelect="onSelectHandler"></Radio>
+ *    </Item>
+ *    <Item>
+ *        <Label>Cherry (secondary color)</Label>
+ *        <Radio value="cherry" color="secondary" @onSelect="onSelectHandler"></Radio>
+ *    </Item>
+ *    <Item>
+ *         <Label>Disabled</Label>
+ *        <Radio value="disabled" :disabled="true" @onSelect="onSelectHandler"></Radio>
+ *    </Item>
+ *    <Item>
+ *        <Label>Default</Label>
+ *        <Radio value="default" @onSelect="onSelectHandler"></Radio>
+ *    </Item>
+ * </List>
+ *
+ **/
 import { isTrueProperty, isPresent } from '../../util/util'
 import ThemeMixins from '../../themes/theme.mixins'
 import IonButton from '../button/index'
@@ -42,6 +104,9 @@ export default {
   watch: {
     disabled (val) {
       this.setDisabled(isTrueProperty(val))
+    },
+    checked (val) {
+      isTrueProperty(val) && this.setChecked(this.value)
     }
   },
   mounted () {
@@ -60,8 +125,8 @@ export default {
       if (this.isChecked !== isChecked) {
         this.isChecked = isChecked
 
-        this.itemCmp.setElementClass('item-radio-checked', isChecked)
-        this.itemCmp.setElementClass(`item-radio-${this.theme}-${this.color}`, isChecked && isPresent(this.color))
+        this.itemCmp.setElementClass('item-radio-checked', this.isChecked)
+        this.itemCmp.setElementClass(`item-radio-${this.theme}-${this.color}`, this.isChecked && isPresent(this.color))
       }
     },
 
